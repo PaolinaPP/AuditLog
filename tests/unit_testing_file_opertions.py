8a8659907d3fade768dd82c74a1cc381473db89f
Paolina Petkova <paolina12123@gmail.com>
2021-04-20 23:01:21 +0300

    Add tests and test xlsx file.

---
+++ tests/unit_testing_file_opertions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import unittest
+from pathlib import Path
+from openpyxl import load_workbook
+import sys
+sys.path.append('..')
+import scripts.file_operations as fo
+
+class TestFileOperations(unittest.TestCase):
+
+    c_file_path = Path(__file__).with_name('info_table.xlsx')
+    c_fo_obj = fo.FileOperations(c_file_path)
+    c_sheet = load_workbook(filename=c_file_path).active
+
+    def test_load_file(self):
+        actual = TestFileOperations.c_fo_obj.load_file()
+        self.assertEqual(TestFileOperations.c_sheet.max_row, actual.max_row)
+        self.assertEqual(TestFileOperations.c_sheet.max_column, actual.max_column)
+
+    def test_read_data(self):
+        expected = ['File: Лекция 8: Език за заявки SPARQL', 'File: Лекция 1: Въведение в програмиране за семантичен уеб', 'File: Лекция 7: Проектиране на онтология с Protégé', 'File: Лекция 9: Програмиране за семантичен уеб', 'File: Лекция 1: Въведение в програмиране за семантичен уеб']
+        actual = TestFileOperations.c_fo_obj.read_data("File: Лекция", TestFileOperations.c_sheet)
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()        
